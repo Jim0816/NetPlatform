1fd66d5b7a8f3ba270649612f93a6436c10c83d0
“jim” <jim164411@163.com>
2022-09-08 14:08:22 +0800

    完成框架搭建

--- src/components/Frame/index.jsx
+++ src/components/Frame/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Table, Modal,Popconfirm } from 'antd';
 import exit from '../../asserts/photo/exit.png'
+import edit from '../../asserts/photo/edit.png'
 import frame from './index.module.css'
 
 
@@ -8,20 +10,80 @@ import { withRouter } from 'react-router-dom'
 // 引入路由配置
 import {adminRoutes} from '../../routes'
 
+const data = [
+  {
+    key: 0,
+    label: `ip`,
+    value: '127.0.0.1',
+    remark: `IP地址`,
+  },
+  {
+    key: 1,
+    label: `mac`,
+    value: 'ff-ff-ff-ff-ff-ff',
+    remark: `mac地址`,
+  }
+];
+
+const columns = [
+  {
+    title: '参数名称',
+    dataIndex: 'label',
+    width: 300,
+  },
+  {
+    title: '参数值',
+    dataIndex: 'value',
+    width: 300,
+    render : 'aaaa ' + 
+  },
+  {
+    title: '备注',
+    dataIndex: 'remark',
+  },
+  {
+    title: 'operation',
+    dataIndex: 'operation',
+    render: (_, record) =>
+      data.length >= 1 ? (
+        <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.key)}>
+          <a>删除</a>
+        </Popconfirm>
+      ) : null,
+  }
+];
+
+
+
 class index extends Component {
 
   state = {
-    relation: {}
+    relation: {},
+    open: false
   }
 
   constructor(props){
     super(props)
     this.state.relation = props.relation
   }
+
+  openDialog = () => {
+    this.state.open = true
+    this.forceUpdate()
+  }
+
+  setOpen = (state) => {
+    this.state.open = state
+    this.forceUpdate()
+  }
+
+  handleDelete = (key) => {
+
+  }
   
   render(){
     var curPath = this.props.location.pathname
-    let {relation} = this.state
+    let {relation, open} = this.state
     var parentPath = relation[curPath].parent
     var childPath = relation[curPath].child
     console.log(parentPath, childPath)
@@ -38,6 +100,25 @@ class index extends Component {
 
     return (
       <div className={frame.app_container}>
+        {/* 对话框 */}
+        <Modal
+          title="网络信息配置"
+          centered
+          visible={open}
+          width={1000}
+          onOk={() => this.setOpen(false)}
+          onCancel={() => this.setOpen(false)}
+        >
+          <Table
+              columns={columns}
+              dataSource={data}
+              pagination={false}
+              scroll={{
+                y: 400,
+              }}
+            />
+        </Modal>
+        
         <div className={frame.app_top}>
           <span className={frame.app_top_title}>网络监测平台</span>
           
@@ -69,12 +150,16 @@ class index extends Component {
                   </div>
                 );
               })} 
+              <span className={frame.app_bottom_left_logo_btn} onClick={this.openDialog}>
+                <img src={edit} style={{height: "100%", width: "100%"}}></img>
+              </span>
             </div>
             {/* 内容区域 */}
             <div className={frame.app_bottom_right}>
               {this.props.children}
             </div>
         </div>
+
       </div>
     )
   }
--- src/pages/Analyse/BusinessAnalyse/index.jsx
+++ src/pages/Analyse/BusinessAnalyse/index.jsx
@@ -1,6 +1,36 @@
 import React, { Component } from 'react'
+import { Space, Table, Tag } from 'antd';
 import { message } from 'antd';
 import business from './index.module.css'
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    width: 150,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    width: 150,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+  },
+];
+
+const data = [];
+
+for (let i = 0; i < 100; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
+
 
 export default class index extends Component {
 
@@ -33,7 +63,18 @@ export default class index extends Component {
             <span className={business.top_tool} ></span>
             <span className={business.top_tool} ></span>
           </div>
-          <div className={business.center}>数据列表: <a href='https://ant.design/components/table-cn/' target="_blank">参考组件</a></div>
+          <div className={business.center}>
+            <Table
+              columns={columns}
+              dataSource={data}
+              pagination={{
+                pageSize: 10,
+              }}
+              scroll={{
+                y: 310,
+              }}
+            />
+          </div>
           <div className={business.bottom_1}>
             <div className={business.bottom_1_left}>
               解码树: <a href='https://blog.csdn.net/weixin_41254345/article/details/117222154' target="_blank">参考组件</a>
